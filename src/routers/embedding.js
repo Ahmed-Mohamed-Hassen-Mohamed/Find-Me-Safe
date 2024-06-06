b38a673ee9b9f3e5a7e79ca9616c3c06d82691e9
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-06-06 13:38:07 +0300

    Update embedding.js

--- src/routers/embedding.js
+++ src/routers/embedding.js
@@ -7,29 +7,29 @@ const userAuth = require("../middelware/userAuth");
 const embedding = require("../controllers/embedding");
 const directory = path.join(__dirname, "../assets/img/images");
 
-if (!fs.existsSync(directory)) {
-  fs.mkdirSync(directory);
-}
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, directory); // Save files to the "src/assets/img/images" directory
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.originalname); // Use the original file name
-    },
-  }),
-  fileFilter: (req, file, cb) => {
-    // Accept only image files
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Invalid file type. Only image files are allowed."));
-    }
-  },
-});
+// if (!fs.existsSync(directory)) {
+//   fs.mkdirSync(directory);
+// }
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, directory); // Save files to the "src/assets/img/images" directory
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, file.originalname); // Use the original file name
+//     },
+//   }),
+//   fileFilter: (req, file, cb) => {
+//     // Accept only image files
+//     if (file.mimetype.startsWith("image/")) {
+//       cb(null, true);
+//     } else {
+//       cb(new Error("Invalid file type. Only image files are allowed."));
+//     }
+//   },
+// });
 
-const _upload = multer({
+const upload = multer({
   fileFilter(req, file, cd) {
     if (!file.originalname.match(/\.(jpg|jpeg|png|jfif)$/)) {
       return cd(new Error("Please upload image"));
@@ -42,11 +42,11 @@ router.post(
   "/addImages",
   userAuth,
   upload.fields([
-    { name: "face", maxCount: 10 },
-    { name: "fingerprint", maxCount: 10 },
+    { name: "face" },
+    { name: "fingerprint" },
   ]),
   embedding.addEmbedding
 );
-router.post("/predict", userAuth, _upload.array("images"), embedding.predict);
+router.post("/predict", userAuth, upload.array("images"), embedding.predict);
 router.get("/retrain", embedding.retrain);
 module.exports = router;
