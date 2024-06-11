d5c011d24ac0c5bd466c96c82266eed92dba4ebb
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-06-11 19:42:54 +0300

    Update notifications.js

--- src/controllers/notifications.js
+++ src/controllers/notifications.js
@@ -6,11 +6,11 @@ exports.getNotifications = async (req, res) => {
   try {
     const userId = req.params.id;
     const notifications = await Notification.find({ userId }).sort({ _id: -1 });
-    if (!notifications.length) {
-      return res
-        .status(404)
-        .send({ Error: "Not found", message: "Not notifications is found" });
-    }
+    // if (!notifications.length) {
+    //   return res
+    //     .status(404)
+    //     .send({ Error: "Not found", message: "Not notifications is found" });
+    // }
     res.status(200).send(notifications);
   } catch (err) {
     res.status(500).send(err);
