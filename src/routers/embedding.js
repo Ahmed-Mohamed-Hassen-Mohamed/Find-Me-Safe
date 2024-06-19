afcd1372c556257e78bb450b4bfa1f2c0fd7c08c
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-06-19 18:26:10 +0300

    Update embedding.js

--- src/routers/embedding.js
+++ src/routers/embedding.js
@@ -2,32 +2,9 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
 const userAuth = require("../middelware/userAuth");
 const embedding = require("../controllers/embedding");
 const directory = path.join(__dirname, "../assets/img/images");
-
-// if (!fs.existsSync(directory)) {
-//   fs.mkdirSync(directory);
-// }
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: (req, file, cb) => {
-//       cb(null, directory); // Save files to the "src/assets/img/images" directory
-//     },
-//     filename: (req, file, cb) => {
-//       cb(null, file.originalname); // Use the original file name
-//     },
-//   }),
-//   fileFilter: (req, file, cb) => {
-//     // Accept only image files
-//     if (file.mimetype.startsWith("image/")) {
-//       cb(null, true);
-//     } else {
-//       cb(new Error("Invalid file type. Only image files are allowed."));
-//     }
-//   },
-// });
 
 const upload = multer({
   fileFilter(req, file, cd) {
@@ -41,12 +18,17 @@ const upload = multer({
 router.post(
   "/addImages",
   userAuth,
-  upload.fields([
-    { name: "face" },
-    { name: "fingerprint" },
-  ]),
+  upload.fields([{ name: "face" }, { name: "fingerprint" }]),
   embedding.addEmbedding
 );
-router.post("/predict", userAuth, upload.array("images"), embedding.predict);
+router.post(
+  "/predict",
+  userAuth,
+  upload.fields([
+    { name: "face", maxCount: 1 },
+    { name: "fingerprint", maxCount: 1 },
+  ]),
+  embedding.predict
+);
 router.get("/retrain", embedding.retrain);
 module.exports = router;
