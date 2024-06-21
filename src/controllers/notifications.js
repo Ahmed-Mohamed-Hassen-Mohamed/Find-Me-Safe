f1b4d7d091f4d9d928f925fab03cd0587bd22603
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-06-22 00:05:32 +0300

    Update notifications.js

--- src/controllers/notifications.js
+++ src/controllers/notifications.js
@@ -5,6 +5,10 @@ const Notification = require("../models/notifications");
 exports.getNotifications = async (req, res) => {
   try {
     const userId = req.params.id;
+    const _notifications = await Notification.updateMany(
+      { userId },
+      { isRead: true }
+    );
     const notifications = await Notification.find({ userId }).sort({ _id: -1 });
     // if (!notifications.length) {
     //   return res
@@ -12,6 +16,20 @@ exports.getNotifications = async (req, res) => {
     //     .send({ Error: "Not found", message: "Not notifications is found" });
     // }
     res.status(200).send(notifications);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.getNotificationsNumber = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    console.log(userId);
+    const notificationsNumber = await Notification.countDocuments({
+      userId,
+      isRead: false,
+    });
+    res.status(200).send({ notificationsNumber });
   } catch (err) {
     res.status(500).send(err);
   }
