1c4787a73cb6266a1e447545a061252164847b85
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-06-22 00:05:58 +0300

    Update notifications.js

--- src/routers/notifications.js
+++ src/routers/notifications.js
@@ -4,5 +4,6 @@ const userAuth = require("../middelware/userAuth");
 const notifications = require("../controllers/notifications");
 
 router.get("/notifications/:id", userAuth, notifications.getNotifications);
+router.get("/notificationsNumber/:id", userAuth, notifications.getNotificationsNumber);
 
 module.exports = router;
