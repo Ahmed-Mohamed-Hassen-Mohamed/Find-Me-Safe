e747383f37f740a11b937e332ea92114198da1ab
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-06-24 01:49:39 +0300

    Update messages.js

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -5,16 +5,16 @@ const Message = require("../models/messages");
 exports.getMessages = async (req, res) => {
   try {
     const chatId = req.params.id;
-    const _messages = await Notification.updateMany(
+    const _messages = await Message.updateMany(
       { chatId },
       { isRead: true }
     );
     const messages = await Message.find({ chatId }).sort({ _id: -1 });
-    // if (!messages.length) {
-    //   return res
-    //     .status(404)
-    //     .send({ Error: "Not found", message: "Not messages is found" });
-    // }
+    if (!messages.length) {
+      return res
+        .status(404)
+        .send({ Error: "Not found", message: "Not messages is found" });
+    }
     res.status(200).send(messages);
   } catch (err) {
     res.status(500).send(err);
