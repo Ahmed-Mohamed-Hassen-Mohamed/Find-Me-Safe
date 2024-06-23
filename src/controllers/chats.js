abe9955318f2b8228bcc7d5859bc2e65015cb90f
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-06-23 22:01:12 +0300

    Update chats.js

--- src/controllers/chats.js
+++ src/controllers/chats.js
@@ -1,36 +1,120 @@
-const Chat = require("../models/chats");
+const Notification = require("./models/notifications");
+const Socket = require("./models/sockets");
+const Chat = require("./models/chats");
+const Message = require("./models/messages");
+const jwt = require("jsonwebtoken");
 
-// Example controller functions
-
-exports.createChat = async (chatData) => {
-  try {
-    const _chat = await Chat.findOne({
-      childId: chatData.childId,
-      finderId: chatData.finderId,
-    });
-    if (!_chat) {
-      const chat = new Chat(chatData);
-      await chat.save();
+exports.socketEvents = (io) => {
+  io.on("connection", async (socket) => {
+    const token = socket.handshake.query.token;
+    if (token) {
+      try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        if (decoded) {
+          socket.userId = decoded._id;
+          let session = await Socket.findOne({ userId: decoded._id });
+          if (!session) {
+            session = new Socket({
+              userId: decoded._id,
+              socketId: socket.id,
+            });
+          }
+          session.socketId = socket.id;
+          await session.save();
+          console.log("A user connected");
+        }
+      } catch (err) {
+        console.error("Token verification failed:", err.message);
+      }
+    } else {
+      console.error("Token is missing");
     }
-  } catch (err) {
-    throw new Error({ message: err });
-  }
+    socket.on("authenticated", (data) => authenticated(data, socket));
+
+    // Listen for notification events
+    // socket.on("notification", (data) => sendNotification(data, io));
+
+    // Listen for chat events
+    socket.on("chat", (data) => handleChat(data, io));
+
+    // Listen for message events
+    socket.on("message", (data) => handleMessage(data, io));
+
+    socket.on("disconnect", () => removeSocket(socket));
+  });
 };
 
-exports.getChats = async (req, res) => {
+async function removeSocket(socket) {
   try {
-    const userId = req.params.id;
-    const chats = await Chat.find({
-      $or: [{ parentId: req.user._id }, { finderId: req.user._id }],
-    }).populate("parentId finderId")
-      .sort({ _id: -1 });
-    // if (!chats.length) {
-    //   return res
-    //     .status(404)
-    //     .send({ Error: "Not found", message: "Not chats is found" });
-    // }
-    res.status(200).send(chats);
+    const socketId = socket.id;
+    await Socket.findOneAndDelete({ socketId });
+    console.log("User disconnected");
   } catch (err) {
-    res.status(500).send(err);
+    console.error(err.message);
+  }
+}
+
+async function authenticated(data, socket) {
+  try {
+    let session = await Socket.findOne({ userId: data.userId });
+    if (!session) {
+      session = new Socket({
+        userId: data.userId,
+        socketId: socket.id,
+      });
+    }
+    session.socketId = socket.id;
+    await session.save();
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function handleChat(chatData, io) {
+  try {
+    const chat = new Chat(chatData);
+    await chat.save();
+
+    const recipientSocket = await Socket.findOne({ userId: chat.parentId });
+    if (recipientSocket) {
+      io.to(recipientSocket.socketId).emit("chat", chat);
+    }
+  } catch (error) {
+    console.error("Error handling chat event:", error);
+  }
+}
+
+async function handleMessage(messageData, io) {
+  try {
+    const message = new Message(messageData);
+    await message.save();
+
+    const recipientSocket = await Socket.findOne({ userId: message.receiverId });
+    if (recipientSocket) {
+      io.to(recipientSocket.socketId).emit("message", message);
+    }
+  } catch (error) {
+    console.error("Error handling message event:", error);
+  }
+}
+
+exports.sendNotification = async (data, io) => {
+  try {
+    const notification = new Notification(data);
+    await notification.save();
+
+    const notificationsNumber = await Notification.countDocuments({
+      userId: data.userId,
+      isRead: false,
+    });
+    const recipientSocket = await Socket.findOne({ userId: notification.userId });
+    if (recipientSocket) {
+      io.to(recipientSocket.socketId).emit("notification", {
+        notification,
+        notificationsNumber,
+      });
+    }
+  } catch (error) {
+    console.error("Error handling notification event:", error);
   }
 };
